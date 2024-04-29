cb4a51e197c227c8394c534208f2defa1ebb0f01
ngphatdat <147393737+ngphatdat@users.noreply.github.com>
2024-04-29 17:01:15 +0700

    upatecode

---
+++ NewsWebAppAPI/Program.cs
@@ -0,0 +1,34 @@
+﻿using NewsWebAppAPI.Models;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
+using NewsWebAppAPI.Repositories;
+using NewsWebAppAPI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var settings = builder.Configuration.GetRequiredSection("ConnectionStrings"); //read data from appsettings. json
+builder.Services.AddDbContext<Database>(options=>
+options.UseSqlServer(settings["DefaultConnection"]));
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
